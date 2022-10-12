2330ea6114e74732234766b9f6277796dffdec6c
Petrovich1973 <designtorg@ya.ru>
2022-10-13 01:06:08 +0300

    создал таблицу процесса в статике

--- src/components/StateIdMegaProcess.js
+++ src/components/StateIdMegaProcess.js
@@ -12,129 +12,63 @@ export default function StateIdMegaProcess() {
                 <tr>
                     <th colSpan="2" rowSpan="2"/>
                     <th style={{backgroundColor: "transparent"}}/>
-                    <th colSpan="3">Main</th>
+                    <th colSpan="11">Main</th>
                     <th style={{backgroundColor: "transparent"}}/>
-                    <th colSpan="3">Stop</th>
+                    <th colSpan="11">Stop</th>
                     <th style={{backgroundColor: "transparent"}}/>
-                    <th colSpan="3">StandIn</th>
+                    <th colSpan="11">StandIn</th>
                 </tr>
-                <tr>
+                <tr className={'rowIdMegas'}>
                     <th style={{backgroundColor: "transparent"}}/>
-                    <th>13</th>
-                    <th>16</th>
-                    <th>38</th>
+                    {state.idMega.map(idMega => (<th key={idMega}>{idMega}</th>))}
                     <th style={{backgroundColor: "transparent"}}/>
-                    <th>13</th>
-                    <th>16</th>
-                    <th>38</th>
+                    {state.idMega.map(idMega => (<th key={idMega}>{idMega}</th>))}
                     <th style={{backgroundColor: "transparent"}}/>
-                    <th>13</th>
-                    <th>16</th>
-                    <th>38</th>
+                    {state.idMega.map(idMega => (<th key={idMega}>{idMega}</th>))}
                 </tr>
                 </thead>
-                <tbody><tr><td colSpan="11" style={{backgroundColor: "transparent"}} height={10}/></tr></tbody>
-                <tbody>
-                <tr>
-                    <td rowSpan="2">FORD</td>
-                    <td>фоновые</td>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td>125</td>
-                    <td>125</td>
-                    <td>125</td>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td/>
-                    <td/>
-                    <td/>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td/>
-                    <td/>
-                    <td/>
-                </tr>
-                <tr>
-                    <td>онлайн</td>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td>125</td>
-                    <td>125</td>
-                    <td>125</td>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td/>
-                    <td/>
-                    <td/>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td/>
-                    <td/>
-                    <td/>
-                </tr>
-                </tbody>
-                <tbody><tr><td colSpan="11" style={{backgroundColor: "transparent"}} height={10}/></tr></tbody>
-                <tbody>
-                <tr>
-                    <td rowSpan="2">MKS</td>
-                    <td>фоновые</td>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td>46</td>
-                    <td>46</td>
-                    <td>46</td>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td/>
-                    <td/>
-                    <td/>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td/>
-                    <td/>
-                    <td/>
-                </tr>
-                <tr>
-                    <td>онлайн</td>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td>46</td>
-                    <td>46</td>
-                    <td>46</td>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td/>
-                    <td/>
-                    <td/>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td/>
-                    <td/>
-                    <td/>
-                </tr>
-                </tbody>
-                <tbody><tr><td colSpan="11" style={{backgroundColor: "transparent"}} height={10}/></tr></tbody>
-                <tbody>
-                <tr>
-                    <td rowSpan="2">COD</td>
-                    <td>фоновые</td>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td>75</td>
-                    <td>75</td>
-                    <td>75</td>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td/>
-                    <td/>
-                    <td/>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td/>
-                    <td/>
-                    <td/>
-                </tr>
-                <tr>
-                    <td>онлайн</td>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td>75</td>
-                    <td>75</td>
-                    <td>75</td>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td/>
-                    <td/>
-                    <td/>
-                    <td style={{backgroundColor: "transparent"}}/>
-                    <td/>
-                    <td/>
-                    <td/>
-                </tr>
-                </tbody>
+                {state.applications.map((application, idx) => (
+                    <>
+                        <tbody>
+                        <tr>
+                            <td colSpan="11" style={{backgroundColor: "transparent"}} height={10}/>
+                        </tr>
+                        </tbody>
+                        <tbody key={idx}>
+                        <tr>
+                            <td rowSpan="2" style={{verticalAlign: "middle", padding: '0 10px', textAlign: "left"}}>{application.name}</td>
+                            <td style={{textAlign: "left", padding: '2px'}}>фоновые</td>
+                            <td style={{backgroundColor: "transparent"}}/>
+                            {application.process.backgroundProcess.map((process, processIdx) => (
+                                <td key={processIdx} style={{padding: '2px'}}>{process.nodeCount}</td>
+                            ))}
+                            <td style={{backgroundColor: "transparent"}}/>
+                            {application.process.backgroundProcess.map((process, processIdx) => (
+                                <td key={processIdx} style={{padding: '2px'}}>{process.nodeCount}</td>
+                            ))}
+                            <td style={{backgroundColor: "transparent"}}/>
+                            {application.process.backgroundProcess.map((process, processIdx) => (
+                                <td key={processIdx} style={{padding: '2px'}}>{process.nodeCount}</td>
+                            ))}
+                        </tr>
+                        <tr>
+                            <td style={{textAlign: "left", padding: '2px'}}>онлайн</td>
+                            <td style={{backgroundColor: "transparent"}}/>
+                            {application.process.onlineProcess.map((process, processIdx) => (
+                                <td key={processIdx} style={{padding: '2px'}}>{process.nodeCount}</td>
+                            ))}
+                            <td style={{backgroundColor: "transparent"}}/>
+                            {application.process.onlineProcess.map((process, processIdx) => (
+                                <td key={processIdx} style={{padding: '2px'}}>{process.nodeCount}</td>
+                            ))}
+                            <td style={{backgroundColor: "transparent"}}/>
+                            {application.process.onlineProcess.map((process, processIdx) => (
+                                <td key={processIdx} style={{padding: '2px'}}>{process.nodeCount}</td>
+                            ))}
+                        </tr>
+                        </tbody>
+                    </>
+                ))}
             </table>
         </div>
     )
